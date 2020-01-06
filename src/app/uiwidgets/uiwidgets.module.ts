4f797bb9e4d289bc49b1de6c0ef457ab50ae9266
Luis Tchitue <ls04af@gmail.com>
2020-01-06 19:11:07 +0100

    commiting the changes before renaming the style files

---
+++ src/app/uiwidgets/uiwidgets.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroComponent } from './components/hero/hero.component';
+
+
+
+@NgModule({
+  declarations: [HeroComponent],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    HeroComponent
+  ]
+})
+export class UIwidgetsModule { }
