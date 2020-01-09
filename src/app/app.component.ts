f72488daf571f2b634b9dd08f3d0c058691a4b3e
Luis Tchitue <ls04af@gmail.com>
2020-01-09 15:42:03 +0100

    custom favicon added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'valdisaPro';
+  title = 'Mossalo : Melhor oportunidade para todos';
 }
